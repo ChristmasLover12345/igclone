5abe9ed76af65db32614e728e3a34470a61b6e9f
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-29 09:56:47 -0700

    Native react practice

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // Div = view
+    // h1, p, etc = Text
+    
+    <SafeAreaView style={styles.container}>
+      <Text>First react native</Text>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
+//This styles is created using a vuilt in component called StyleSheet that allows us to style our components
+
+// flex 1 takes up all of the available space of the parent component
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
