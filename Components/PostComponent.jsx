c39470d098bf31d4ee201437f7a7ede1ef746f64
ChristmasLover12345 <madnes0205@gmail.com>
2025-05-07 09:53:34 -0700

    adding some posts

--- Components/PostComponent.jsx
+++ Components/PostComponent.jsx
@@ -1,10 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+
+import React, { useState } from 'react'
+import StoriesComponent from './StoriesComponent'
+import elonJump from '../assets/elonJump.jpg'
+import MePicture from '../assets/MePicture.png'
 
 export default function PostComponent() {
+
+  const [post, setPost] = useState([
+  {
+    profilePicture: MePicture,
+    usernName: "Sigma",
+    postImage: elonJump,
+    numOfLikes: "21",
+    description: "Elon Musk Jumping in the Air",
+    comments: [
+      {
+        userCommentor: "iHATEPeople",
+        comment: "I hate people who jump in the air like this",
+      }
+    ]
+  },
+  {
+    profilePicture: elonJump,
+    usernName: "Elon Xusk",
+    postImage: MePicture,
+    numOfLikes: "-28",
+    description: "why does he look like that",
+    comments: [
+      {
+        userCommentor: "PickleRick",
+        comment: "morty gues what",
+      }
+    ]
+  }
+  ])
+
+ 
   return (
-    <View>
-      <Text>PostComponent</Text>
+    <View >
+      <StoriesComponent/>
+      <Text style={{color: "white"}}>PostComponent</Text>
     </View>
   )
 }
--- Components/StoriesComponent.jsx
+++ Components/StoriesComponent.jsx
@@ -1,12 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import MePicture from '../assets/MePicture.png'
 
 export default function StoriesComponent() {
   return (
-    <View>
-      <Text>StoriesComponent</Text>
-    </View>
+    // We are creating a Carousel byy adding horizontal to your scroll view Component
+    <ScrollView horizontal>
+      
+      {/* Container that holds our stories */}
+      <View style={styles.container}>
+        {/* Gradiant Border Around our image */}
+        <LinearGradient style={styles.linerGradeant} colors={["#feda75", "#Fede1e", "#d62976", "#962fbf", "#4f5bd5"]}>
+
+        {/* This View will be our image Wrapper*/}
+        <View style={styles.imageWrapper}>
+          {/* Profile Image */}
+          <Image  source={MePicture} style={styles.image}/>
+        </View>
+
+        </LinearGradient>
+        <Text style={styles.LabelText}>Sigma</Text>
+      </View>
+
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    margin: 10
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 50
+  },
+  imageWrapper: {
+    width: 87,
+    height: 87,
+    borderRadius: 50,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  linerGradeant: {
+    width: 94,
+    height: 94,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  LabelText: {
+    color: "white",
+    fontSize: 15,
+    marginTop:5,
+  }
+
+
+})
